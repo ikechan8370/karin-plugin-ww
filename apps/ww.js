9309beb55f69482a2d6a326536f3d3418eadd6c9
ikechan8370 <geyinchi@buaa.edu.cn>
2024-05-26 22:10:44 +0800

    feat: 角色

--- apps/ww.js
+++ apps/ww.js
@@ -2,6 +2,107 @@ import { common, plugin, Renderer, segment } from '#Karin'
 import { KuroUser } from '../model/user.js'
 import { KuroClient } from '../model/kuro.js'
 
+const characters =
+  {
+    1102: {
+      name: '散华',
+      alias: ['散花', 'Sanhua']
+    },
+    1103: {
+      name: '白芷',
+      alias: ['白纸', 'Baizhi']
+    },
+    1104: {
+      name: '凌阳',
+      alias: ['雪豹', 'Lingyang']
+    },
+    1202: {
+      name: '炽霞',
+      alias: ['赤霞', 'Chixia']
+    },
+    1203: {
+      name: '安可',
+      alias: ['安妮', 'Encore']
+    },
+    1204: {
+      name: '莫特斐',
+      alias: ['Mortefi']
+    },
+    1301: {
+      name: '卡卡罗',
+      alias: ['calcharo', 'kakarot', '可可萝']
+    },
+    1302: {
+      name: '吟霖',
+      alias: ['银临', '银铃', 'Yinlin']
+    },
+    1303: {
+      name: '渊武',
+      alias: ['元武', 'Yuanwu']
+    },
+    1304: {},
+    1402: {
+      name: '秧秧',
+      alias: ['泱泱']
+    },
+    1403: {
+      name: '秋水',
+      alias: []
+    },
+    1404: {
+      name: '忌炎',
+      alias: ['鸡眼', 'Jiyan']
+    },
+    1405: {
+      name: '鉴心',
+      alias: ['Jianxin', '小道', '剑心']
+    },
+    1501: {
+      name: '漂泊者-衍射',
+      alias: ['漂泊者']
+    },
+    1502: {
+      name: '漂泊者-衍射',
+      alias: ['漂泊者']
+    },
+    1503: {
+      name: '维里奈',
+      alias: ['维里', '维里奈', 'Verina']
+    },
+    1504: {
+      name: '漂泊者-导电',
+      alias: ['漂泊者']
+    },
+    1505: {
+      name: '漂泊者-导电',
+      alias: ['漂泊者']
+    },
+    1601: {
+      name: '桃祈',
+      alias: ['淘气', 'Taoqi']
+    },
+    1602: {
+      name: '丹瑾',
+      alias: ['丹静', 'Danjin']
+    },
+    1603: {
+      name: '椿',
+      alias: ['Chun', 'Camellya']
+    },
+    1604: {
+      name: '漂泊者-湮灭',
+      alias: ['漂泊者']
+    },
+    1605: {
+      name: '漂泊者-湮灭',
+      alias: ['漂泊者']
+    }
+  }
+const chatReg = Object.values(characters).flatMap(i => {
+  let r = i.alias || []
+  r.push(i.name)
+  return r
+}).filter(i => i).join('|')
 export class WutheringWaves extends plugin {
   constructor () {
     super({
@@ -20,6 +121,12 @@ export class WutheringWaves extends plugin {
           reg: '^(#鸣潮|ww|#ww)(\\d{9})?$',
           /** 执行方法 */
           fnc: 'basicWW'
+        },
+        {
+          /** 命令正则匹配 */
+          reg: `#?(${chatReg})`,
+          /** 执行方法 */
+          fnc: 'role'
         },
         {
           /** 命令正则匹配 */
@@ -135,5 +242,63 @@ export class WutheringWaves extends plugin {
       logger.error(data)
     }
     this.reply('绑定失败')
+  }
+
+  async role () {
+    let msg = this.e.msg
+    let role = msg.replace(/#/g, '')
+    const uin = this.e.sender.uin
+    const user = KuroUser.get(uin)
+    if (!user) {
+      await this.reply('请先绑库街区uid', { reply: true })
+      return
+    }
+    if (user.tokens.length === 0) {
+      // 尝试用全局token查询
+      let allUsers = KuroUser.getAll()
+      for (let key in allUsers) {
+        if (allUsers[key].tokens.length > 0) {
+          user.tokens = allUsers[key].tokens
+          break
+        }
+      }
+    }
+    const client = new KuroClient(user.tokens[0])
+    let roles = await client.sendRequest('role', user.wwUid[0])
+    if (!roles.ok) {
+      await this.reply('获取信息失败', { reply: true })
+      logger.error(roles.data)
+      return
+    }
+    let roleId = 0
+    for (let char of Object.keys(characters)) {
+      if (characters[char].alias?.includes(role) && roles.data.roleList?.find(role => role.roleId === parseInt(char))) {
+        roleId = char
+        break
+      }
+    }
+    if (!roleId) {
+      return
+    }
+
+    let res = await client.sendRequest('roleDetail', user.wwUid[0], { id: roleId })
+    if (!res.ok) {
+      await this.reply('获取信息失败', { reply: true })
+      logger.error(res.data)
+      return
+    }
+    let roleDetail = res.data
+    const filePath = common.absPath('./plugins/karin-plugin-ww/resources')
+    const html = filePath + '/template/role/index.html'
+    let renderData = {
+      data: roleDetail,
+      tplFile: html,
+      pluResPath: process.cwd() + '/plugins/karin-plugin-ww/resources'
+    }
+    const img = await Renderer.render({
+      name: 'render',
+      data: renderData
+    })
+    this.reply(segment.image(img), { reply: true })
   }
 }
